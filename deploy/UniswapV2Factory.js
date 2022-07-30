9238c4df93648378173c39c4bf7961019819bb7d
Navil Rodrigues <rodriguesnavil@gmail.com>
2022-07-31 01:18:27 +0530

    Lint deploy scripts

--- deploy/UniswapV2Factory.js
+++ deploy/UniswapV2Factory.js
@@ -1,29 +1,20 @@
-const {
-    bytecode,
-    abi,
-  } = require("../deployments/mainnet/UniswapV2Factory.json");
-  
-  module.exports = async function ({
-    ethers,
-    getNamedAccounts,
-    deployments,
-    getChainId,
-  }) {
-    const { deploy } = deployments;
-  
-    const { deployer, dev } = await getNamedAccounts();
-  
+const { bytecode, abi } = require("../deployments/mainnet/UniswapV2Factory.json")
+
+module.exports = async function ({ ethers, getNamedAccounts, deployments, getChainId }) {
+    const { deploy } = deployments
+
+    const { deployer, dev } = await getNamedAccounts()
+
     await deploy("UniswapV2Factory", {
-      contract: {
-        abi,
-        bytecode,
-      },
-      from: deployer,
-      args: [dev],
-      log: true,
-      deterministicDeployment: false,
-    });
-  };
-  
-  module.exports.tags = ["UniswapV2Factory", "AMM"];
-  
+        contract: {
+            abi,
+            bytecode,
+        },
+        from: deployer,
+        args: [dev],
+        log: true,
+        deterministicDeployment: false,
+    })
+}
+
+module.exports.tags = ["UniswapV2Factory", "AMM"]
